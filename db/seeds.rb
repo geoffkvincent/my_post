3461e8ca9b9b1e9fe355c9cb2e2575405501cb10
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-19 16:25:04 -0600

    added routes, controller, seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+categories = ['Games', 'Health & Fitness', 'Business', 'Lifestyle', 'Entertainment', 'Sports']
+
+
+20.times do
+  App.create(
+    name: Faker::App.name,
+    body: Faker::Lorem.paragraph(4),
+    category: categories.sample,
+  )
+end
