5f326787aebc60c2dbd079c02b04e2f337f8306a
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-19 16:37:44 -0600

    seed fix, server works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 categories = ['Games', 'Health & Fitness', 'Business', 'Lifestyle', 'Entertainment', 'Sports']
 
 
-20.times do
-  App.create(
-    name: Faker::App.name,
-    body: Faker::Lorem.paragraph(4),
+25.times do
+  Post.create(
+    name: Faker::Hipster.words,
+    body: Faker::Simpsons.quote,
     category: categories.sample,
   )
 end
