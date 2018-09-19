8775d7a06701cba6d570791e028cd76fa63819d4
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-19 17:11:00 -0600

    home, posts page works, added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import Home from './components/Home';
 import Posts from './components/Posts';
 import Post from './components/Post';
+import NavBar from './components/NavBar';
 import NoMatch from './components/NoMatch';
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/posts" component={Posts} />
-    <Route exact path="/posts/:id" component={Post} />
-    <Route component={NoMatch} />
-  </Switch>
+  <Fragment>
+    <NavBar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/posts" component={Posts} />
+      <Route exact path="/posts/:id" component={Post} />
+      <Route component={NoMatch} />
+    </Switch>
+  </Fragment>
 )
 
 
