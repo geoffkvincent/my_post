1df4b6578d4cd4b0d246ee6cec76b088c776bb67
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-19 18:25:56 -0600

    filter by category

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -2,15 +2,32 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getPosts } from '../reducers/posts';
-import { Container, Grid, Header, Card, Image } from 'semantic-ui-react';
+import { Container, Grid, Header, Card, Image, Dropdown, Divider, Button } from 'semantic-ui-react';
 
 class Posts extends React.Component {
+
+  state = { category: ''}
+
+  
+
   componentDidMount() {
     this.props.dispatch(getPosts())
   }
 
+  categoryOptions = () => {
+    const { categories } = this.props
+    return categories.map( (c) => { return { key: c, text: c, value: c } } )
+  }
+
   posts = () => {
-    return this.props.posts.map( post =>
+    const { posts } = this.props
+    const { category } = this.state
+
+    let visible = posts
+
+    if (category) 
+      visible = posts.filter( p => p.category === category )
+    return visible.map( post =>
       <Card key={post.id}>
         
         <Card.Content>
@@ -31,9 +48,20 @@ class Posts extends React.Component {
   }
 
   render() {
+    let { category } = this.state;
     return (
       <Container>
         <Header as="h3" textAlign="center">Posts</Header>
+        <Dropdown
+          placeholder="Filter by category"
+          fluid
+          selection
+          options={this.categoryOptions()}
+          onChange={ (e, data) => this.setState({ category: data.value }) }
+          value={category}
+        />
+       { category && <Button fluid basic onClick={ () => this.setState({ category: '' }) }>Clear Filter: {category}</Button> }
+       <Divider />
           <Card.Group itemsPerRow={4}>
            { this.posts() }
           </Card.Group>
@@ -43,7 +71,10 @@ class Posts extends React.Component {
   }
 
   const mapStateToProps = (state) => {
-    return { posts: state.posts }
+    const posts = state.posts;
+    const categories = [...new Set(posts.map( p => p.category ))]
+    return { posts, categories}
   }
+  
 
 export default connect(mapStateToProps)(Posts);
