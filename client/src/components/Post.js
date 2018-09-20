0686d925fb5cc07e44b71d8712c2916b89f59f15
Geoff Vincent <geoffkvincent@gmail.com>
2018-09-19 18:38:14 -0600

    post view page

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Divider, Header, Image, Container, Table } from 'semantic-ui-react';
 
-const Posts = () => (
-  <div>Posts</div>
+
+const Post = ({ post = {} }) => (
+  <Container>
+    
+    <Header as="h3" textAlign="center">{post.name}</Header>
+    
+    <Table definition>
+      <Table.Header>
+        
+      </Table.Header>
+
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>Description</Table.Cell>
+          <Table.Cell>{post.body}</Table.Cell>
+        </Table.Row>
+        
+        <Table.Row>
+          <Table.Cell>Category</Table.Cell>
+          <Table.Cell>{post.category}</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  </Container>
 )
 
-export default Posts;
+const mapStateToProps = (state, props) => {
+  return { post: state.posts.find( p => p.id === parseInt(props.match.params.id )) }
+}
+
+export default connect(mapStateToProps)(Post);
